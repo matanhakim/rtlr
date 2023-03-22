8852f00c1cf0cacf7c571b0ae71af1c90a5921ac
Matan <matanhakim@gmail.com>
2023-03-22 18:22:17 +0200

    Add initial documentation for the package

--- R/str_rtl.R
+++ R/str_rtl.R
@@ -1,3 +1,19 @@
+#' Wrap a string with RTL-embedding Unicode characters
+#'
+#' @param ... One or more character vectors, ideally of length 1.
+#' @param multiline Whether to split the input strings to separate lines. Defaults to `FALSE`.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' # Notice how the `.` prints to the right, while it should print to the left
+#' cat("א.")
+#'
+#' # `str_rtl()` fixes this:
+#' str_rtl("א.") |> cat()
+#'
+#' str_rtl("א.", "ב.", multiline = TRUE) |> cat()
 str_rtl <- function(..., multiline = FALSE) {
   if (multiline)
   {
