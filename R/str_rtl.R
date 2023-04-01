d2e1b44778546542343f162a097c801cfd03cd14
Matan <matanhakim@gmail.com>
2023-04-01 13:12:25 +0300

    Add str_replace all that replaces \n with \n\u202B
    This is currently not working, as the unlist(list(...))
    is producing a vector, rather than a list of vectors.

--- R/str_rtl.R
+++ R/str_rtl.R
@@ -17,10 +17,25 @@
 str_rtl <- function(..., multiline = FALSE) {
   if (multiline)
   {
-    stringr::str_c("\u202B", stringr::str_c(..., sep = "\n\u202B", collapse = "\n\u202B"))
+    stringr::str_c(
+      "\u202B",
+      stringr::str_c(
+        unlist(list(...)) |>
+          stringr::str_replace_all("\n", "\n\u202B"),
+        sep = "\n\u202B",
+        collapse = "\n\u202B"
+      )
+    )
   }
   else
   {
-    stringr::str_c("\u202B", stringr::str_c(..., sep = "\u202B"))
+    stringr::str_c(
+      "\u202B",
+      stringr::str_c(
+        unlist(list(...)) |>
+          stringr::str_replace_all("\n", "\n\u202B"),
+        sep = "\u202B"
+      )
+    )
   }
 }
